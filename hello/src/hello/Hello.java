5507d7512bb30cec300e998e940cb58aaf15cccc
rahmatullah <rhmt171217@gmail.com>
2023-02-08 19:34:38 +0700

    Update Hello.java

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -1,11 +1,9 @@
 
 package hello;
-
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        
+        // TODO code application logic here
     }
     
 }
